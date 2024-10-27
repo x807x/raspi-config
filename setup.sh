18befdad233ba94b3ae2ee1efbacfff7fca655ae
807 <s10855168@gmail.com>
2024-10-27 12:05:47 +0800

    cargo fix

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@ sudo apt update && sudo apt upgrade -y && sudo apt full-upgrade -y
 
 sudo apt install "vim git curl wget" -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
 
 cat dotfiles/.bashrc >> ~/.bashrc
 cat dotfiles/.bash_aliases >> ~/.bash_aliases
 
 cargo install eza --locked
+cargo install zoxide --locked
